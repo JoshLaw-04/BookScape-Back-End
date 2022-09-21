d85597d5bc43665f39569554f9bff5c87b8acc46
Bill Franklin <franklinbk316@gmail.com>
2022-09-21 05:54:20 -0500

    worked on userRoutes

---
+++ src/controllers/userController.ts
@@ -0,0 +1,10 @@
+import { RequestHandler } from "express";
+import { User } from "../models/user";
+
+export const createUser: RequestHandler = async (req, res, next) => {
+
+}
+
+export const loginUser: RequestHandler = async (req, res, next) => {
+
+}
