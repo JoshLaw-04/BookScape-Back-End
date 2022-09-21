edf9e1baba09cbebc9e82b13260cf32e4e9fdd64
hellok8ie <k8iewatson@yahoo.com>
2022-09-21 15:06:44 -0400

    Added getReview function & updated getAllReviews to include a SQL filter

--- src/controllers/reviewController.ts
+++ src/controllers/reviewController.ts
@@ -1,10 +1,43 @@
 import { RequestHandler } from "express";
 import { Review } from "../models/review";
+import { User } from "../models/user";
+import { verifyUser } from "../services/auth";
 
 export const getAllReviews: RequestHandler = async (req,res,next) => {
-    //we will need to add an "include" constraint in .findAll() to control for
-    //allowing logged in users to view all of their own reviews
-    let reviews = await Review.findAll();
+    let reviews = await Review.findAll( {include: [{
+                                                model: User, 
+                                                required: true}],
+                                                order: [['reviewId', 'DESC']]});
     res.status(200).json(reviews);
 };
+
+export const createReview: RequestHandler = async (req, res, next) => {
+    let user: User | null = await verifyUser(req);
+
+    if (!user) {
+        return res.status(403).send();
+    }
+
+    let newReview: Review = req.body;
+    newReview.userId = user.userId;
+
+    if (newReview.comment) {
+        let created = await Review.create(newReview);
+        res.status(201).json(created)
+    } else {
+        res.status(400).send('Please add a comment');
+    }
+};
+
+export const getReview: RequestHandler = async (req, res, next) => {
+    let reviewId = req.params.reviewId;
+    let foundReview = await Review.findByPk(reviewId);
+    if (foundReview) {
+        res.status(200).json(foundReview);
+    } else {
+        res.status(404).json({});
+    }
+};
+
+
 
