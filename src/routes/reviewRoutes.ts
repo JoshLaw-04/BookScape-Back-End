c24098aec15dfb454fd700ac7611b9f3735d90c9
hellok8ie <k8iewatson@yahoo.com>
2022-09-21 14:46:23 -0400

    Added reviewRoutes

---
+++ src/routes/reviewRoutes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+
+const router = Router();
+
+//GET localhost:3000/api/reviews
+router.get('/', getAllReviews);
+
+//POST localhots:3000/api/review
+router.post('/', createReview);
+
+//GET localhost:3000/api/reviews/:reviewId
+router.get('/:reviewId', getReview);
+
+//PUT localhost:3000/api/reviews/:reviewId
+router.put('/:reviewId', updateReview);
+
+//DELETE localhost:3000/api/reviews/:reviewId
+router.delete('/:reviewId', deleteReview);
+
+export default router;
